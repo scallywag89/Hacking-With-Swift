57c262fe24683b1c095652435d2bc8b8703a67be
Bennett Mackenzie <90129223+scallywag89@users.noreply.github.com>
2022-12-03 10:23:28 +1100

    Coded hide bars on tap
    - Added override functions to the DetailViewController

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -22,6 +22,16 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
 
     /*
     // MARK: - Navigation
