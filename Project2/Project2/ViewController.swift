cf719ad602d99d89a2a183c24a731680bff5562b
Bennett Mackenzie <90129223+scallywag89@users.noreply.github.com>
2022-12-03 16:19:09 +1100

    Project 2
    - Added images to asset catalog
    - created buttons
    - linked buttons to ViewController

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Bennett Mackenzie on 3/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
