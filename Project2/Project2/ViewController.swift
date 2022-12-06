ecfa6eed58740bde7716dbad90b2e886bf53b128
Bennett Mackenzie <90129223+scallywag89@users.noreply.github.com>
2022-12-06 22:52:41 +1100

    Finished Project 2
    Fun!

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var correctAnswer = 0
     var score = 0
+    var askedQuestions = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         countries += ["estonia", "france", "germany",
@@ -42,7 +43,15 @@ class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+        title = "\(countries[correctAnswer].uppercased()) || Score: \(score)"
+        askedQuestions += 1
+    }
+    
+    func reset(action: UIAlertAction) {
+        score = 0
+        askedQuestions = 0
+        
+        askQuestion(action: nil)
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
@@ -52,12 +61,19 @@ class ViewController: UIViewController {
             title = "Correct"
             score += 1
         } else {
-            title = "Wrong"
+            title = "Wrong, that is \(countries[sender.tag].uppercased())'s flag!"
             score -= 1
         }
-        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-        present(ac, animated: true)
+        
+        if askedQuestions < 10 {
+            let alertController = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(alertController, animated: true)
+        } else {
+            let secondAlertController = UIAlertController(title: "Game Over", message: "You got \(score)/10", preferredStyle: .alert)
+            secondAlertController.addAction(UIAlertAction(title: title, style: .default, handler: reset))
+            present(secondAlertController, animated: true)
+        }
     }
     
 }
